43fb65a2c7b833876b7b01037a1ab2167bb59335
Tahir Abbas <46996800+tahir098@users.noreply.github.com>
2019-09-09 16:20:03 +0500

    Added a button to view profile and arranged the content which is displayed

--- js/main.js
+++ js/main.js
@@ -19,16 +19,17 @@ $('#profile').html(
                 //    "<b>Public repos : </b>" + (user.public_repos) 
                 
                 '<div class="card" style="width: 20rem;">' + 
-                '<img src="'+user.avatar_url+'" + class="card-img-top" +</img>' +
+                '<img src="'+user.avatar_url+'" + id="img" class="card-img-top img" +</img>' +
                 '<div class="card-body">' +
-                '<h5 class="card-title"> <b>Name : </b>'    + user.name + '</h5>' +
-                '<div class="card-header"> <b>Login : </b>' + user.login +'</div>' +
-                '<div class="card-body"> <b>Location : </b>'  + user.location+ "</div> " +
-                '<b> Email : </b>' + user.email+ "</div> " +
-                '<b>Created at : </b>'  + user.created_at+ "</div> " +
-                '<b>Company : </b>'  + user.company+ "</div>" + '</br>' +
-                '<b>Public repos : </b>'  + user.public_repos+ "</div> " + '<br>' +
-                '</div>'
+                '<h5 class="card-header ml-3"> <b>Name : </b>'+ user.name + '</h5>' +
+               // '<div class="card-header"> <b>Login : </b>' + user.login +'</div>' +
+                '<div class="card-body"><b>Location : </b>' + user.location + '<hr>' +
+                '<b> Email : </b>'+user.email + '<hr>' +
+                '<b> Bio : </b> '+user.bio +'<hr>' +
+                  '<b>Created at : </b>'+ user.created_at +'<hr>' +
+                '<b>Company : </b>'+ user.company + '<hr>' +
+                '<b>Public repos : </b>'+ user.public_repos+ '<hr>' +
+                '<a href="'+user.html_url+'" class="btn btn-primary btn-block">View profile</a></div><br></div>'
    
 
        //  email,location,created_at,company,public_repos,
